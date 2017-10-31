a1c2711b6783764ad8b2de8c2ad8d747caae5ef1
jonathan <jonathan.araya.m@gmail.com>
2017-10-31 14:11:09 -0300

    [B] iniciando twitter al levantar el server

---
+++ server/config/twitterCredentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         '41ORB4GCgUxXWNcmTLAsjpYDv',
+    consumer_secret:      'QpG17l93KDlQWHKpdeNsXqLaJiZ3eeTc7BIb1CBuS9IaIctpxE',
+    access_token:         '83757941-vJvzEgz4DsOQGbuopm52BeEhvVGzcdQrWWjfGO8oF',
+    access_token_secret:  'mzjDo4YV7yWxTzAcnvrbnOT0oqkLOQgz10hMPPLwzpKXx',
+    timeout_ms:           60*1000
+}
